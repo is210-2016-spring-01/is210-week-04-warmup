abf2cac6efaa3236d855ae5f7aac3b6952727fad
williamjcole <billy.cole@gmail.com>
2016-02-16 00:02:40 +0000

    added POSITIONAL

--- task_02.py
+++ task_02.py
@@ -4,3 +4,13 @@
 
 
 import hamlet
+POSITIONAL = hamlet.crazy_math(4, 100000, 8, 98)
+""" First this code imports the functions from hamlet.py This function takes
+    the integers 4, 100000, 8 and 98 then passes them through the crazy_math
+    function. And then takes the results and stores them in the POSITIONAL
+    parameter.
+
+    Example:
+    >>> print (task_02.POSITIONAL)
+    0.00377496083577474
+"""
