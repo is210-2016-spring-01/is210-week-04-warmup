1a1996797b55ea5d535a0fecf65638eeebe5259e
williamjcole <billy.cole@gmail.com>
2016-02-16 00:04:20 +0000

    Added too_many_kittens and documentation

--- task_03.py
+++ task_03.py
@@ -4,3 +4,12 @@
 
 
 import hamlet
+KEYWORD = hamlet.crazy_math(bananas=48, monkeys=84, hours=200000)
+""" First this code imports the functions from hamlet.py. This function takes
+    the parameters bananas, monkeys, and hours, gives them the values 48, 84
+    and then 200000 then passes them through the crazy_math function. And then
+    takes the results and stores them in the POSITIONAL parameter
+
+    Example:
+    >>> print (task_03.KEYWORD)
+    0.008791685104370117 """
