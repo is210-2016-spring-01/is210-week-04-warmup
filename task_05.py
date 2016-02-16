24efe5c5b651bb6f1ce2a28d8e4c7e60704e5fb1
williamjcole <billy.cole@gmail.com>
2016-02-16 00:06:39 +0000

    added Default and documentation

---
+++ task_05.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Defaults"""
+
+
+def defaults(my_required, my_optional=True):
+
+    """This function tests if the default coincides with the correct
+    parameter
+
+    Arguments:
+    my_required(Boolean): This parameter does not have a default value.
+    my_optional(Boolean): This parameter has the value of True.
+
+    Example:
+    >>> defaults(True)
+    True
+    >>> defaults(True, False)
+    False
+    >>> defaults(False, False)
+    True
+
+    """
+
+    return my_optional is my_required
