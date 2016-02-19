68038036fbf0f3154289edba19637ca146cd8056
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-02-19 02:34:55 +0000

    Created a def docstring

--- task_01.py
+++ task_01.py
@@ -4,6 +4,20 @@
 
 
 def know_what_i_mean(wink, numwink=2):
+    """Does some math and returns a string.
+
+    Args:
+        wink (mixed): Arg to be arithmetically multiplied with numwink
+        numwink (int): Defined number. Default: 2
+
+    Returns:
+        int: All arguements mutiplied with commas
+
+    Examples:
+
+        >>>
+    """
+
     winks = (wink * numwink).strip()
     nudges = ('nudge ' * numwink).strip()
     retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
