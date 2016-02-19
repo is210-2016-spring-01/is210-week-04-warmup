93829c4875bb29980d0fda975915e84ca27cfb3f
Spencer <SpencerGallard@gmail.com>
2016-02-19 03:28:36 +0000

    Task04 for Warm up Week 04.

---
+++ task_04.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module does some pretty crazy math."""
+
+def too_many_kittens(kittens,litterboxes,catfood):
+    """ """
+    value = not (litterboxes >= kittens and catfood)
+    return value
