66b08665d456d62cde49b61a2850dae54ad829ca
paisely65 <spscuny@gmail.com>
2016-02-19 04:22:00 +0000

    Both tasks are completed.

--- task_01.py
+++ task_01.py
@@ -10,17 +10,16 @@ def know_what_i_mean(wink, numwink=2):
 
     Args:
         winks (mixed): Argument to be multiplied with numwink.
-        numwink (int, optional): Argument to  multiply with nudge. Default: 2
-        nudges (str): Argument to be multiplied with numwink.
+        numwink (int, optional): Argument to multiply with nudge. Default: 2
 
     Returns:
         str: All argumets concatenated with commas.
 
     Examples:
-    
+
         know_what_I_mean('winkwink', 'nudgenudge')
     """
-    winks = (wink * numwink).strip()
-    nudges = ('nudge ' * numwink).strip()
-    retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
+    winks = (wink * numwink)
+    nudges = ('nudge' * numwink)
+    retstr = 'know_what_i_mean'.format(winks, nudges)
     return retstr
