4932b513e065e817dac0494c0f007e2772d21ebc
Sylvia Guzikowski <xiran-aneki@live.com>
2016-02-19 08:24:57 +0000

    homework done!

--- task_01.py
+++ task_01.py
@@ -4,6 +4,30 @@
 
 
 def know_what_i_mean(wink, numwink=2):
+    """Flirts with the person running the code.
+
+    There would be a paragraph here, explaining what's happening
+    but it's optional, so for now I'll only aim for three lines
+    to take up a nice piece of space.
+
+    Args:
+        wink (str): Arg that winks the number of times specified in numwink
+        numwink (int): Arg specifies how many times wink is printed. Default = 2
+
+    Returns:
+        str: Arguments put together with number of winks and nudges specified
+        by the user. 
+
+        Examples:
+
+        >>> know_what_i_mean(3, 5)
+        'Know what I mean? wink wink wink wink wink, nudge
+         nudge nudge nudge nudge'
+
+        >>> know_what_i_mean(3)
+        'Know what I mean? wink wink, nudge nudge'       
+    """
+
     winks = (wink * numwink).strip()
     nudges = ('nudge ' * numwink).strip()
     retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
---
+++ task_05.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+def defaults(my_required, my_optional=True):
+    """This function determines something true or false.
+
+    Args:
+        my_optional(bool): has a default value of True
+	my_required(bool): is a required parameter with no default value
+
+    Returns:
+        bool: wether or not a statement is true
+
+    Examples:
+
+        >>>defaults(True)
+	True
+		
+	>>>defaults(True, False)
+	False
+		
+	>>>defaults(False, False)
+	True
+    """
+    return my_optional is my_required
