37d0e539d603e312a93181c021ee30632f40915a
Nicole Parrilla <nicole.parrilla@spsmail.cuny.edu>
2016-02-21 21:42:31 +0000

    Fixed documentation

--- task_01.py
+++ task_01.py
@@ -4,6 +4,21 @@
 
 
 def know_what_i_mean(wink, numwink=2):
+    """ This function knows what you mean
+
+    Args:
+        wink (str): arg to be multipled with numwink
+        numwink (int): arg to implement winks and nudges. Default: 2
+
+    Returns:
+        str: All arguments to be concatenated with commas
+
+    Examples:
+        >>> know_what_i_mean(3, numwink)
+        '3, 2'
+
+    """
+
     winks = (wink * numwink).strip()
     nudges = ('nudge ' * numwink).strip()
     retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
