fd60e09c6468b7bfb223d2bf3a8c3e9fc36d4031
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-03-01 00:41:39 +0000

    Fixed formatting

--- task_03.py
+++ task_03.py
@@ -5,12 +5,4 @@
 
 import hamlet
 
-
-hamlet.crazy_math(bananas=48, monkeys=84, hours=200000)
-
-{'bananas':48, 'monkeys':84, 'hours':200000}
-
-
-KEYWORD = 0.008791685104370117
-
-print KEYWORD
+KEYWORD = hamlet.crazy_math(bananas=48, monkeys=84, hours=200000)
